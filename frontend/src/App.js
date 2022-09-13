dc4e061f5337336e241fc0d9b872d220dcb2104c
Rossen27 <jetso1127@gmail.com>
2022-09-13 15:58:24 +0800

    Update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,13 +8,17 @@ function App() {
       </header>
       <main>
         <h1>Featured Products</h1>
+        <div className="products">
         {
-          data.products.map(product => (<div>
+          data.products.map(product => (<div className="product" key={product.slug}>
             <img src={product.image} alt={product.name} />
-            <p>{product.name}</p>
-            <p>{product.price}</p>
+              <div className="product-info">
+                <p>{product.name}</p>
+                <p>{product.price}</p>
+              </div>
           </div>))
         }
+        </div>
       </main>
     </div>
   );
