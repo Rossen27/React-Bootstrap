25bf70cebe62f054c0b8bd669a21944f07e7d1f0
Rossen27 <jetso1127@gmail.com>
2022-09-19 09:03:54 +0800

    建立購物車頁面

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -4,67 +4,93 @@ import { Helmet } from 'react-helmet-async';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import MessageBox from '../components/MessageBox';
-import { Link } from 'react-router-dom';
 import ListGroup from 'react-bootstrap/ListGroup';
 import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import { Link } from 'react-router-dom';
 
 export default function CartScreen() {
   const { state, dispatch: ctxDispatch } = useContext(Store);
   const {
-    cart: { cartItem },
+    cart: { cartItems },
   } = state;
 
   return (
     <div>
       <Helmet>
-        <title>購物車</title>
+        <title>Shopping Cart</title>
       </Helmet>
-      <h1>購物車</h1>
+      <h1>Shopping Cart</h1>
       <Row>
         <Col md={8}>
-          {
-            cartItem.length === 0 ? (
-              <MessageBox>
-                Cart is empty. <Link to="/">繼續購物</Link>
-              </MessageBox>
-            ) :
-              (
-                <ListGroup>
-                  {cartItem.map((item) => (
-                    <ListGroup.Item key={item._id}>
-                      <Row className='align=items-canter'>
-                        <Col md={4}>
-                          <img
-                            src={item.image}
-                            alt={item.name}
-                            className="img-fluid rounded img-thumbnail"
-                          ></img>{' '}
-                          <Link to={`/product/${item.slug}`}>{item.name}</Link>
-                        </Col>
-                        <Col md={3}>
-                          <Button variant="light" disabled={item.quantity === 1}>
-                            <i className='fas fa-minus-circle'></i>
-                          </Button>{' '}
-                          <span>{item.quantity}</span>{' '}
-                          <Button variant="light" disabled={item.quantity === 1}>
-                            <i className='fas fa-plus-circle'></i>
-                          </Button>
-                        </Col>
-                        <Col md={3}>${item.price}</Col>
-                        <Col md={2}>
-                          <Button variant="light">
-                            <i className='fas fa-trash'></i>
-                          </Button>
-                        </Col>
-                      </Row>
-                    </ListGroup.Item>
-                  ))}
-                </ListGroup>
-              )
-          }
+          {cartItems.length === 0 ? (
+            <MessageBox>
+              Cart is empty. <Link to="/">Go Shopping</Link>
+            </MessageBox>
+          ) : (
+            <ListGroup>
+              {cartItems.map((item) => (
+                <ListGroup.Item key={item._id}>
+                  <Row className="align-items-center">
+                    <Col md={4}>
+                      <img
+                        src={item.image}
+                        alt={item.name}
+                        className="img-fluid rounded img-thumbnail"
+                      ></img>{' '}
+                      <Link to={`/product/${item.slug}`}>{item.name}</Link>
+                    </Col>
+                    <Col md={3}>
+                      <Button variant="light" disabled={item.quantity === 1}>
+                        <i className="fas fa-minus-circle"></i>
+                      </Button>{' '}
+                      <span>{item.quantity}</span>{' '}
+                      <Button
+                        variant="light"
+                        disabled={item.quantity === item.countInStock}
+                      >
+                        <i className="fas fa-plus-circle"></i>
+                      </Button>
+                    </Col>
+                    <Col md={3}>${item.price}</Col>
+                    <Col md={2}>
+                      <Button variant="light">
+                        <i className="fas fa-trash"></i>
+                      </Button>
+                    </Col>
+                  </Row>
+                </ListGroup.Item>
+              ))}
+            </ListGroup>
+          )}
         </Col>
-        <Col md={4}></Col>
+        <Col md={4}>
+          <Card>
+            <Card.Body>
+              <ListGroup variant="flush">
+                <ListGroup.Item>
+                  <h3>
+                    總計 ({cartItems.reduce((a, c) => a + c.quantity, 0)}{' '}
+                    件商品) : $
+                    {cartItems.reduce((a, c) => a + c.price * c.quantity, 0)}
+                  </h3>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <div className="d-grid">
+                    <Button
+                      type="button"
+                      variant="primary"
+                      disabled={cartItems.length === 0}
+                    >
+                      結帳
+                    </Button>
+                  </div>
+                </ListGroup.Item>
+              </ListGroup>
+            </Card.Body>
+          </Card>
+        </Col>
       </Row>
     </div>
   );
-};
+}
