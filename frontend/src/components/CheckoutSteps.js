38184011fd1d750bd68d1321be173714d30ad397
Rossen27 <jetso1127@gmail.com>
2022-09-20 16:29:25 +0800

    建立訂單頁面
    1. 顯示購物車物品、付款和地址資料
    2. 處理下訂單動作
    3. 創建訂單 api

--- frontend/src/components/CheckoutSteps.js
+++ frontend/src/components/CheckoutSteps.js
@@ -7,8 +7,8 @@ export default function CheckoutSteps(props) {
     <Row className="checkout-steps">
       <Col className={props.step1 ? 'active' : ''}>登 入</Col>
       <Col className={props.step2 ? 'active' : ''}>購 物 車</Col>
-      <Col className={props.step3 ? 'active' : ''}>結 帳</Col>
-      <Col className={props.step4 ? 'active' : ''}>繼 續 購 物</Col>
+      <Col className={props.step3 ? 'active' : ''}>付 款 方 式</Col>
+      <Col className={props.step4 ? 'active' : ''}>結 帳</Col>
     </Row>
   );
 }
